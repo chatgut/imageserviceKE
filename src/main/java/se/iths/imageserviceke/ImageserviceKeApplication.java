ce75ea45e5f7ea83f9c0e42113174fc14ba25329
altmannk <katherine.altmann@iths.se>
2024-05-03 09:56:25 +0200

    Initial commit
    Co-authored-by: Emil Winther <emil.winther@hotmail.com>

---
+++ src/main/java/se/iths/imageserviceke/ImageserviceKeApplication.java
@@ -0,0 +1,13 @@
+package se.iths.imageserviceke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageserviceKeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageserviceKeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/imageserviceke/ImageserviceKeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.imageserviceke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageserviceKeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
